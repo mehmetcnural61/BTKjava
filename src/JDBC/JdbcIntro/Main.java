6ded00356851d2b664bc87c666dfd5f1f0c304be
mehmetcnural61 <mehmetnural61@hotmail.com>
2022-11-24 23:17:57 +0300

    Insert

--- src/JDBC/JdbcIntro/Main.java
+++ src/JDBC/JdbcIntro/Main.java
@@ -1,11 +1,48 @@
 package JDBC.JdbcIntro;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
 
 
     public static void main(String[] args) throws SQLException {
+
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
+
+
+        //İfade, Cümleciğimiz
+        PreparedStatement statement = null;
+        ResultSet resultSet;
+
+
+        try {
+            connection = helper.getConnection();
+            String sql= "insert into city (Name,CountryCode,District,Population)values (?,?,?,?)";
+            statement = connection.prepareStatement(sql);
+            statement.setString(1,"İzmir");
+            statement.setString(2,"TUR");
+            statement.setString(3,"Turkey");
+            statement.setInt(4,35000);
+
+
+
+            //how many rows affected
+            int result = statement.executeUpdate();
+
+            System.out.println("Kayıt Eklendi");
+
+        } catch (SQLException exception) {
+            helper.showErrorMessage(exception);
+        } finally {
+            statement.close();
+            connection.close();
+        }
+
+    }
+
+    public static void selectDemo() throws SQLException {
         DbHelper helper = new DbHelper();
         Connection connection = null;
 
@@ -18,15 +55,22 @@ public class Main {
             connection = helper.getConnection();
             statement = connection.createStatement();
             resultSet = statement.executeQuery("select code,Name,Continent,Region from Country");
-            while (resultSet.next()){
-                System.out.println(resultSet.getString("Name"));
+            ArrayList<Country> countries = new ArrayList<Country>();
+            while (resultSet.next()) {
+                countries.add(new Country(
+                        resultSet.getString("Code"),
+                        resultSet.getString("Name"),
+                        resultSet.getString("Continent"),
+                        resultSet.getString("Region")));
             }
+
+            System.out.println(countries.size());
+
         } catch (SQLException exception) {
             helper.showErrorMessage(exception);
         } finally {
             connection.close();
         }
-
-
     }
 }
+
