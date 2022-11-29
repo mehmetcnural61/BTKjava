b6576c439ebe4beee55da99af1112fecf3f4a392
mehmetcnural61 <mehmetnural61@hotmail.com>
2022-11-29 14:39:18 +0300

    JDBC DataBase & MySql

--- src/JDBC/JdbcIntro/Main.java
+++ src/JDBC/JdbcIntro/Main.java
@@ -12,26 +12,16 @@ public class Main {
         DbHelper helper = new DbHelper();
 
 
-        //İfade, Cümleciğimiz
         PreparedStatement statement = null;
         ResultSet resultSet;
 
 
         try {
             connection = helper.getConnection();
-            String sql= "insert into city (Name,CountryCode,District,Population)values (?,?,?,?)";
-            statement = connection.prepareStatement(sql);
-            statement.setString(1,"İzmir");
-            statement.setString(2,"TUR");
-            statement.setString(3,"Turkey");
-            statement.setInt(4,35000);
-
-
-
-            //how many rows affected
+            String sql="delete from city where name='izmir' ";
+            statement=connection.prepareStatement(sql);
             int result = statement.executeUpdate();
-
-            System.out.println("Kayıt Eklendi");
+            System.out.println("Kayıt Silindi !");
 
         } catch (SQLException exception) {
             helper.showErrorMessage(exception);
@@ -69,6 +59,64 @@ public class Main {
         } catch (SQLException exception) {
             helper.showErrorMessage(exception);
         } finally {
+            connection.close();
+        }
+    }
+    public static void insertData()throws SQLException{
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
+
+
+        //İfade, Cümleciğimiz
+        PreparedStatement statement = null;
+        ResultSet resultSet;
+
+
+        try {
+            connection = helper.getConnection();
+            String sql= "insert into city (Name,CountryCode,District,Population)values (?,?,?,?)";
+            statement = connection.prepareStatement(sql);
+            statement.setString(1,"İzmir");
+            statement.setString(2,"TUR");
+            statement.setString(3,"Turkey");
+            statement.setInt(4,35000);
+
+
+
+            //how many rows affected
+            int result = statement.executeUpdate();
+
+            System.out.println("Kayıt Eklendi");
+
+        } catch (SQLException exception) {
+            helper.showErrorMessage(exception);
+        } finally {
+            statement.close();
+            connection.close();
+        }
+    }
+    public void updateData()throws SQLException{
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
+
+
+        PreparedStatement statement = null;
+        ResultSet resultSet;
+
+
+        try {
+            connection = helper.getConnection();
+            String sql="update city set population=61000,district='EGE' ";
+            // String sql="update city set population=61000,district='EGE' where id=? ";
+            statement=connection.prepareStatement(sql);
+            // statement.setInt(1,4082);
+            int result = statement.executeUpdate();
+            System.out.println("Kayıt Güncellendi !");
+
+        } catch (SQLException exception) {
+            helper.showErrorMessage(exception);
+        } finally {
+            statement.close();
             connection.close();
         }
     }
